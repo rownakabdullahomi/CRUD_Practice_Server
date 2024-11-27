dbacaeb8282e06710c9f4227237a3ab049cfdfa3
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-27 14:43:36 +0600

    create and read operation done

--- index.js
+++ index.js
@@ -28,9 +28,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //  await client.connect();
+
+    const usersCollection = client.db("usersDB").collection("users");
+
+    // Create
+    app.post("/addUser", async(req, res)=>{
+      console.log(req.body);
+      const result = await usersCollection.insertOne(req.body);
+      res.send(result);
+    })
+
+    // Read
+    app.get("/user", async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+    
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -43,10 +67,10 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Server is Running");
+  res.send("Server is Running");
 })
 
-app.listen(port, ()=>{
-    console.log(`Server is running on post: ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on post: ${port}`);
 })
 
